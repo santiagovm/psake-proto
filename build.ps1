93e5f52c78f451b69df6fae7b301e345ad86ead6
Santi <santiago@egifter.com>
2016-07-20 14:56:18 -0400

    Added some feedback while running and fixed nuget restore provlem

--- build.ps1
+++ build.ps1
@@ -1,13 +1,15 @@
 ﻿cls
 
-# Restore NuGet packages
+# Restore NuGet packages for build to run
+Write-Host "Restoring packages needed for Build script to run"
+
 # SANTI: MAKE THIS A PARAMETER
 $nugetExe = "D:\Santi\soft-lib\NuGet-3\nuget.exe"
 
-# restoring psake package if missing
 & $nugetExe restore ".\Build\packages.config" -PackagesDirectory ".\packages"
 
 # '[p]sake' is the same as 'psake' but $Error is not polluted
+Write-Host "Importing psake module"
 Remove-Module [p]sake
 
 # find psake's path
@@ -17,15 +19,20 @@ $psakeModule = (Get-ChildItem(".\packages\psake.*\tools\psake.psm1")).FullName |
 
 Import-Module $psakeModule
 
+# running the build script
+Write-Host "Running the build script"
+
 Invoke-psake -buildFile .\Build\default.ps1 `
 			 -taskList Test `
 			 -framework 4.5.2 `
 			 -properties @{ 
 			     "buildConfiguration" = "Release" 
 			     "buildPlatform" = "Any CPU" } `
-			 -parameters @{ "solutionFile" = "..\psake.sln" }
-
-Write-Host "`r`nBuild exit code: " $LASTEXITCODE
+			 -parameters @{ 
+				 "solutionFile" = "..\psake.sln" 
+				 "nugetExe" = $nugetExe }
 
 # propagating the exit code so that builds actually fail when there is a problem
+Write-Host "`r`nBuild exit code: " $LASTEXITCODE
+
 Exit $LASTEXITCODE
