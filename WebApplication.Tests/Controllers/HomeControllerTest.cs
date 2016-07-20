0f27359db64af4542e4d3b0d3829063360354eff
Santi <santiago@egifter.com>
2016-07-20 09:35:20 -0400

    Made initial commit

---
+++ WebApplication.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication.Controllers;
+
+namespace WebApplication.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+    }
+}
---
+++ build.ps1
@@ -0,0 +1,23 @@
+﻿cls
+
+# '[p]sake' is the same as 'psake' but $Error is not polluted
+Remove-Module [p]sake
+
+# find psake's path
+$psakeModule = (Get-ChildItem(".\packages\psake.*\tools\psake.psm1")).FullName | Sort-Object $_ | select -Last 1
+
+Import-Module $psakeModule
+
+Invoke-psake -buildFile .\Build\default.ps1 `
+			 -taskList Test `
+			 -framework 4.5.2 `
+			 -properties @{ 
+				 "testMessage" = "this is some custom test message injected as property" 
+			     "buildConfiguration" = "Release" 
+			     "buildPlatform" = "Any CPU" } `
+			 -parameters @{ "solutionFile" = "..\psake.sln" }
+
+Write-Host "`r`nBuild exit code: " $LASTEXITCODE
+
+# propagating the exit code so that builds actually fail when there is a problem
+exit $LASTEXITCODE
